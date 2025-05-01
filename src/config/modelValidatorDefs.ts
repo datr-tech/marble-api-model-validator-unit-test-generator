a0a6fadd7c5edc5abd8bee81c74543bfce453190
joealdersonstrachan <joealdersonstrachan@strachan.email>
2025-05-01 15:25:58 +0100

    feat: update model validator defs to include all apis

--- src/config/modelValidatorDefs.ts
+++ src/config/modelValidatorDefs.ts
@@ -40,6 +40,58 @@ export const modelValidatorDefs: IModelValidatorDefs = {
       positive: 'modelValidatorPositiveTestTemplate',
     },
   },
+  modelValidatorHopId: {
+    idFieldNames: {
+      modelValidator: 'hopId',
+      service: 'hopId',
+    },
+    serviceMethodName: 'hasHop',
+    serviceName: 'dolomiteService',
+    serviceMockName: 'dolomiteService',
+    templates: {
+      negative: 'modelValidatorNegativeTestTemplate',
+      positive: 'modelValidatorPositiveTestTemplate',
+    },
+  },
+  modelValidatorJourneyId: {
+    idFieldNames: {
+      modelValidator: 'journeyId',
+      service: 'journeyId',
+    },
+    serviceMethodName: 'hasJourney',
+    serviceName: 'dolomiteService',
+    serviceMockName: 'dolomiteService',
+    templates: {
+      negative: 'modelValidatorNegativeTestTemplate',
+      positive: 'modelValidatorPositiveTestTemplate',
+    },
+  },
+  modelValidatorOrganisationId: {
+    idFieldNames: {
+      modelValidator: 'organisationId',
+      service: 'organisationId',
+    },
+    serviceMethodName: 'hasOrganisation',
+    serviceName: 'personaService',
+    serviceMockName: 'personaService',
+    templates: {
+      negative: 'modelValidatorNegativeTestTemplate',
+      positive: 'modelValidatorPositiveTestTemplate',
+    },
+  },
+  modelValidatorOwnerUserId: {
+    idFieldNames: {
+      modelValidator: 'ownerUserId',
+      service: 'userId',
+    },
+    serviceMethodName: 'hasUser',
+    serviceName: 'personaService',
+    serviceMockName: 'personaService',
+    templates: {
+      negative: 'modelValidatorNegativeTestTemplate',
+      positive: 'modelValidatorPositiveTestTemplate',
+    },
+  },
   modelValidatorResourceId: {
     idFieldNames: {
       modelValidator: 'resourceId',
@@ -48,6 +100,19 @@ export const modelValidatorDefs: IModelValidatorDefs = {
     serviceMethodName: 'hasResource',
     serviceName: 'entityService',
     serviceMockName: 'entityService',
+    templates: {
+      negative: 'modelValidatorNegativeTestTemplate',
+      positive: 'modelValidatorPositiveTestTemplate',
+    },
+  },
+  modelValidatorUserId: {
+    idFieldNames: {
+      modelValidator: 'userId',
+      service: 'userId',
+    },
+    serviceMethodName: 'hasUser',
+    serviceName: 'personaService',
+    serviceMockName: 'personaService',
     templates: {
       negative: 'modelValidatorNegativeTestTemplate',
       positive: 'modelValidatorPositiveTestTemplate',
